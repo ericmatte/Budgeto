2e9f8be1cc53646c7d2372132a8be1741cd1825b
Eric Matte <eric.matte@usherbrooke.ca>
2016-09-03 15:39:17 -0400

    Android test activities

--- app/AndroidTest/Budgeto/app/src/main/java/com/endless/budgeto/MainActivity.java
+++ app/AndroidTest/Budgeto/app/src/main/java/com/endless/budgeto/MainActivity.java
@@ -1,10 +1,11 @@
 package com.endless.budgeto;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,9 +13,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -61,6 +64,25 @@ public class MainActivity extends AppCompatActivity
                 totalTextView.setText(Float.toString(total));
             }
         });
+    }
+
+    public void showGreetings(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+
+        String IdAsString = view.getResources().getResourceName(view.getId());
+        Toast toast = Toast.makeText(context, IdAsString, duration);
+        toast.show();
+
+        if (IdAsString.endsWith("secActBtn")) {
+            Intent intent = new Intent(this,SecondActivity.class);
+            startActivity(intent);
+        }
+        else if (IdAsString.endsWith("thiActBtn")) {
+            Intent intent = new Intent(this,ThirdActivity.class);
+            startActivity(intent);
+        }
     }
 
     @Override
---
+++ app/AndroidTest/Budgeto/app/src/main/java/com/endless/budgeto/ThirdActivity.java
@@ -0,0 +1,16 @@
+package com.endless.budgeto;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Eric on 2016-09-03.
+ */
+public class ThirdActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_activity);
+    }
+}
