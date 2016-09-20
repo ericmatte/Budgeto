a19b852a3243c05cc26d6383b1e5755514609d1a
Eric Matte <eric.matte@usherbrooke.ca>
2016-09-20 00:25:35 -0400

    Background color transition.

--- app/Budgeto/app/src/main/java/com/endless/budgeto/SetupActivity.java
+++ app/Budgeto/app/src/main/java/com/endless/budgeto/SetupActivity.java
@@ -1,21 +1,16 @@
 package com.endless.budgeto;
 
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.animation.ArgbEvaluator;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
 
 public class SetupActivity extends AppCompatActivity {
@@ -29,6 +24,7 @@ public class SetupActivity extends AppCompatActivity {
      * {@link android.support.v4.app.FragmentStatePagerAdapter}.
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
+    ArgbEvaluator argbEvaluator = new ArgbEvaluator();
 
     /**
      * The {@link ViewPager} that will host the section contents.
@@ -50,44 +46,27 @@ public class SetupActivity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-
-      FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-      fab.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-              Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                      .setAction("Action", null).show();
-          }
-      });
-
+        mViewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            public void onPageScrollStateChanged(int state) {}
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                backgroundColorTransition(position, positionOffset);
+            }
+            public void onPageSelected(int position) {}
+        });
     }
 
+    private void backgroundColorTransition(int position, float positionOffset) {
+        Integer color1 = getResources().getColor(R.color.colorWelcomeBack);
+        Integer color2 = getResources().getColor(R.color.colorBanksBack);
+        Integer color3 = getResources().getColor(R.color.colorAllSetBack);
+        Integer[] colors = {color1, color2, color3};
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_setup, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(position < (mSectionsPagerAdapter.getCount() -1) && position < (colors.length - 1)) {
+            mViewPager.setBackgroundColor((Integer) argbEvaluator.evaluate(positionOffset, colors[position], colors[position + 1]));
+        } else {
+            mViewPager.setBackgroundColor(colors[colors.length - 1]);
         }
-
-        return super.onOptionsItemSelected(item);
     }
-
-
-
-
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
