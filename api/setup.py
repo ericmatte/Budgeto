6997790ad2d2edb954ff68aee9987d79ae3859fa
Eric Matte <eric.matte@usherbrooke.ca>
2016-10-13 22:08:02 -0400

    Removed python-env from project.

---
+++ api/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='Flask API',
+    version='1.0',
+    long_description=__doc__,
+    packages=['flask-api'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Flask>=0.11.1',
+        'Jinja2>=2.8',
+        'MarkupSafe>=0.23',
+        'Werkzeug>=0.11.10',
+        'click>=6.6',
+        'itsdangerous>=0.24',
+    ]
+)
