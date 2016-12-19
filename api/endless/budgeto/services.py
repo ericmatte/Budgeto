8a80312cf9f4365afb237d99594c622556fe2e78
Ian Lachance <ian.lachance14@gmail.com>
2016-12-19 17:15:05 -0500

    Added get-transaction + started to write test for budgeto

--- api/endless/budgeto/services.py
+++ api/endless/budgeto/services.py
@@ -1,22 +1,37 @@
 from flask import request
+from werkzeug.exceptions import BadRequestKeyError
+
+from endless import db_session
 from endless.budgeto import budgeto_services
+from lib.response_handler import HttpResponse, HttpErrorResponse
 from models import Bank,  Transaction, User, Category
 
 
 @budgeto_services.route('/get-transactions', methods=['GET'])
 def get_transactions():
-    return '<b>TRANSACTION COMPLETE</b>'
-
-
-@budgeto_services.route('/set-transactions', methods=['POST'])
-def get_transactions():
     data = request.form
     user = User.by_email(data['email'])
-    for transaction in data['transactions']:
-        trans = Transaction()
-        trans.user_id = user.id
-        trans.bank_id = Bank.by_name(transaction['bank']).id
-        trans.category_id = Category.by_name(transaction.get('cat', 'Other')).id
-        trans.description = transaction['desc']
-        trans.amount = transaction['amount']
-        trans.date = transaction['date']
+    return HttpResponse(Transaction.by_user_id(user.id))
+
+
+# Fonctionne si le device envoye seulement les nouvelles transactions
+@budgeto_services.route('/set-transactions', methods=['POST'])
+def get_transactions():
+    try:
+        data = request.form
+        user = User.by_email(data['email'])
+        for transaction in data['transactions']:
+            trans = Transaction()
+            trans.user_id = user.id
+            trans.bank_id = Bank.by_name(transaction['bank']).id
+            trans.category_id = Category.by_name(transaction.get('cat', 'Unknown')).id
+            trans.description = transaction['desc']
+            trans.amount = transaction['amount']
+            trans.date = transaction['date']
+            db_session.add(trans)
+
+        db_session.commit()
+        return HttpResponse('Transactions received!')
+    except BadRequestKeyError as e:
+        HttpErrorResponse(e)  # Log the error
+        return HttpResponse('A field is missing: ' + str(', '.join(e.args)), status=400)
---
+++ api/tests/test_budgeto.py
@@ -0,0 +1,4 @@
+def test_get_transaction(dummy_transaction):
+    trans = dummy_transaction.__dict__
+    print('test')
+    assert True
