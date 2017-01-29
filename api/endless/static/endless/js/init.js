e66ff973beba74b957c3a1a6e1ffb99a4d1114b5
Eric Matte <eric.matte@usherbrooke.ca>
2017-01-29 18:09:45 -0500

    Fixed Signout button.

--- api/endless/static/endless/js/init.js
+++ api/endless/static/endless/js/init.js
@@ -20,25 +20,3 @@ endless = {
         });
 	}
 }
-
-
-/* Google Sign in */
-function onLoad() {
-    gapi.load('auth2', function() {
-        gapi.auth2.init();
-    });
-}
-
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        $.ajax({
-            url: "{{ url_for('main_services.logout') }}",
-            method: 'post'
-        }).done(function() {
-            console.log('User signed out.');
-            location.reload()
-        });
-    });
-}
-/* End of Google Sign in */
