e27bd8a0316f2d38ee8eca403515427c067e9327
Eric Matte <eric.matte@usherbrooke.ca>
2017-01-30 18:31:58 -0500

    Modal for add new transaction.

--- api/endless/static/endless/js/init.js
+++ api/endless/static/endless/js/init.js
@@ -1,7 +1,21 @@
 $(function() {
-    $('.button-collapse').sideNav();
+    // Plugin initialization
     $('.side-nav').perfectScrollbar();
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    $('.carousel').carousel();
+    $('.slider').slider();
+    $('.parallax').parallax();
+    $('.modal').modal();
+    $('.scrollspy').scrollSpy();
+    $('.button-collapse').sideNav({'edge': 'left'});
+    $('.datepicker').pickadate({selectYears: 20});
+    $('select').not('.disabled').material_select();
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
 });
+
 
 
 endless = {
