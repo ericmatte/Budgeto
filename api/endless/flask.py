6abc66ccacc7e346fd23d3dba1fac5e0138278b7
Eric Matte <eric.matte@usherbrooke.ca>
2017-03-26 23:02:44 -0400

    Path.

--- api/endless/flask.py
+++ api/endless/flask.py
@@ -7,6 +7,7 @@ from endless.server.database import init_db
 app = Flask(__name__)
 selected_config_file = ('../../deployment_config/config_{0}.cfg'
                         .format('prod' if os.environ.get('MODE', '') == 'PROD' else 'debug'))
+app.config["PROJECT_ROOT"] = os.path.abspath(os.path.dirname(__file__))
 app.config.from_pyfile(selected_config_file)
 app.secret_key = 'f3c82c4584527c7ba650b18c38237cca'
 
--- api/endless/main/controllers.py
+++ api/endless/main/controllers.py
@@ -1,5 +1,6 @@
 import codecs
 import json
+import os
 from datetime import datetime
 from os import listdir
 from os.path import isfile, join
@@ -7,7 +8,8 @@ from flask import redirect
 from flask import render_template
 from flask import url_for
 from endless.main import main
-from endless.main.services import login_required, required_roles
+from endless.flask import app
+
 
 
 @main.route('/', methods=['GET'])
@@ -17,7 +19,11 @@ def index():
 
 @main.route('/portfolio', methods=['GET'])
 def portfolio():
-    data = json.load(codecs.open('endless/static/portfolio/data.json', 'r', 'utf-8-sig'))
+    root = app.config["PROJECT_ROOT"]
+    print("WORKING DIRECTORY")
+    print(os.path.dirname(os.path.realpath(__file__)))
+
+    data = json.load(codecs.open(root + '/static/portfolio/data.json', 'r', 'utf-8-sig'))
 
     for i in range(len(data['pictures'])):
         data['pictures'][i]["url"] = "/static/portfolio/pictures/" + data['pictures'][i]["filename"]
@@ -27,7 +33,7 @@ def portfolio():
         data['projects'][i]['id'] = 'project-' + str(i)
         img_path = data['projects'][i].get('images_path')
         if img_path:
-            data['projects'][i]['images'] = [img_path+f for f in listdir('endless'+img_path) if isfile(join('endless'+img_path, f))]
+            data['projects'][i]['images'] = [img_path+f for f in listdir(root + img_path) if isfile(join('endless'+img_path, f))]
 
     return render_template('portfolio.html',
                            title="Eric Matte - Portfolio",
