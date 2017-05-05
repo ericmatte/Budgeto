67950621d47ba213d1e6d32f20ce3b4fa897f835
Eric Matte <eric.matte@usherbrooke.ca>
2017-05-05 17:19:21 -0400

    Moar tests.

---
+++ api/endless/budgeto/backend.py
@@ -0,0 +1,11 @@
+
+
+def put_transactions(json, user):
+    """Save a list of transactions into the database
+    Args:
+        json : str The json containing the bank and the transactions to add
+        user : User The user to add to
+    Returns:
+        The number of transactions added, -1 if an error has happened
+    """
+    pass
--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -1,9 +1,15 @@
+import codecs
+import json
+from datetime import datetime
+
 from flask import g
 from flask import url_for
 
 from endless import app
 from endless import db_session
+from endless.budgeto.backend import put_transactions
 from endless.main.services import update_user
+from models import Transaction
 from models import User
 from tests import set_current_user
 
@@ -38,3 +44,19 @@ def test_update_user(client, dummy_google_user_info):
     user = User.get(google_id=dummy_google_user_info['sub'])
     assert user.full_name == dummy_google_user_info['name']
     assert user.email == new_email
+
+def test_put_transaction(client, statements):
+    with set_current_user():
+        for statement in statements:
+            with codecs.open(statement['json'], 'r', encoding='utf8') as f:
+                ref = json.loads(f.read())
+                now = datetime.now()
+
+                assert put_transactions(statement, g.user)
+
+                transactions = Transaction.get_all(Transaction.upload_time > now)
+                assert len(transactions) == len(ref['transactions'])
+                for i in range(0, len(transactions)):
+                    assert transactions[i].description == ref['transactions']['description']
+                    assert transactions[i].amount == ref['transactions']['amount']
+                    assert transactions[i].date == ref['transactions']['date']
