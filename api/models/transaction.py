85c1355a6ffea118c835b84399fddb0aaf197dd3
Eric Matte <eric.matte@usherbrooke.ca>
2017-05-11 16:51:36 -0400

    Send user data on token validation.

--- api/models/transaction.py
+++ api/models/transaction.py
@@ -33,44 +33,5 @@ class Transaction(DeclarativeBase, BaseEntity):
     user = relationship('User')
     bank = relationship('Bank')
 
-    @classmethod
-    def generate_uuid(cls, user_id, bank_id, amount, date, **kargs):
-        uuid_string = "%d %d %d %s" % (user_id, bank_id, amount, date.strftime("%d-%m-%Y"))
-        return str(uuid.uuid5(uuid.NAMESPACE_DNS, uuid_string))
-
-    @classmethod
-    def get_all_hierarchically(cls, user):
-        """Get all the categories in a hierarchical way"""
-        get_category_transactions = lambda category_id, transactions: [t for t in transactions if t.category_id == category_id]
-
-        def get_children(parent_id, categories, transactions):
-            node_categories = {}
-            for category in categories:
-                if category.parent_id == parent_id:
-                    children_categories = get_children(category.category_id, categories, transactions)
-                    node_transactions = get_category_transactions(category.category_id, transactions)
-                    keywords_count = len(node_transactions) + sum([c['count'] for k, c in children_categories.items()])
-                    node_categories[category.category_id] = {'category': category,
-                                                             'limit': Limit.get(category=category, user=user),
-                                                             'children': children_categories,
-                                                             'count': keywords_count,
-                                                             'transactions': node_transactions}
-            return node_categories
-
-        categories = Category.get_all()
-        return get_children(None, categories, user.transactions)
-
-    @classmethod
-    def get_all_by_bank(cls, user):
-        """Get all the categories in a hierarchical way"""
-        from models import to_json
-        banks = [to_json(bank, False) for bank in Bank.get_all()]
-        transactions = user.transactions
-
-        for bank in banks:
-            bank['transactions'] = to_json([t for t in transactions if t.bank_id == bank['bank_id']], False)
-        return banks
-
-
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
