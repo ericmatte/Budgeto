04dc799a2f5b445694bd20ffb2a9bfa7dde98991
Eric Matte <14026234@usherbrooke.ca>
2018-01-13 14:49:27 -0500

    Updated repo.

--- Backend/api/endless/main/controllers.py
+++ Backend/api/endless/main/controllers.py
@@ -10,37 +10,6 @@ from flask import url_for
 from endless.main import main
 from endless.flask import app
 
-
-
-@main.route('/', methods=['GET'])
-def index():
-    return redirect(url_for('main.portfolio'))
-
-
-@main.route('/portfolio', methods=['GET'])
-def portfolio():
-    root = app.config["PROJECT_ROOT"]
-    data = json.load(codecs.open(root + '/static/portfolio/data.json', 'r', 'utf-8-sig'))
-
-    for i in range(len(data['pictures'])):
-        data['pictures'][i]["url"] = "/static/portfolio/pictures/" + data['pictures'][i]["filename"]
-        data['pictures'][i]["thumbnail"] = "/static/portfolio/pictures/thumbnails/" + data['pictures'][i]["filename"]
-
-    for i in range(len(data['projects'])):
-        data['projects'][i]['id'] = 'project-' + str(i)
-        img_path = data['projects'][i].get('images_path')
-        if img_path:
-            data['projects'][i]['images'] = [img_path+f for f in listdir(root + img_path) if isfile(join(root +img_path, f))]
-
-    return render_template('portfolio.html',
-                           title="Eric Matte - Portfolio",
-                           description="This is my personal portfolio. It contains everything that I am very proud to share from what I\'ve done so far.",
-                           image='/static/portfolio/my-icon.png',
-                           data=data,
-                           color='#034b7b',
-                           now=datetime.utcnow())
-
-
 @main.route('/login', methods=['GET'])
 def login():
     return render_template('login.html', title='Login to Budgeto')
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Endless-Budgeto
+# Budgeto
+![Budgeto](https://ericmatte.me/assets/portfolio/projects/budgeto.png)
+
+Budgeto is a simple webapp that easily help you manage your money.
+
+It let you quickly have an idea of what you mostly spend during the year. You can also set and track limits on every category available.
+
+**Backend**: Made in Python using Flask and Jinja2.
+
+**Frontend**: Made with Angular4.
